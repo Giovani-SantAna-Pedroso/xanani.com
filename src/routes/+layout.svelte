d78627dd718dc02615414987efd61b650cebe92a
Giovani Sant'Ana Ferreira Pedroso Vaz <giovanisfpv@gmail.com>
2025-10-21 14:36:43 -0300

    ini

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import '../app.css';
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+  <link
+    rel="stylesheet"
+    href="https://fonts.googleapis.com/css2?family=Oswald:wght@300;400;500;600;700&display=swap"
+  />
+</svelte:head>
+
+{@render children?.()}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<main class="flex min-h-screen min-w-screen items-center justify-center bg-[#F8A92B]">
+	<p class="font-oswald mt-10 text-center text-2xl">
+		🚧 This website is currently under reformulation.
+	</p>
+</main>
